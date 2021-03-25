f2a48e87d25b774dbce42cdf496cce1a965491ee
Pugazh Krishna <pugazh@powerofn.in>
2021-03-25 16:26:03 +0530

    login design

--- src/app/pre-login/login/login.component.ts
+++ src/app/pre-login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  disableLoginButton: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
+  login() {
+    this.disableLoginButton = true;
+    const input = this.loginForm.value;
+    return input
+  }
 }
