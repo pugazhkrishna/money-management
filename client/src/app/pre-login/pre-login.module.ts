cdbabdca255f72efe51a75058d4aad735c2bd648
Pugazh Krishna <pugazh@powerofn.in>
2021-04-06 11:16:04 +0530

    client and server in same branch

--- client/src/app/pre-login/pre-login.module.ts
+++ client/src/app/pre-login/pre-login.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { PreLoginRoutingModule } from './pre-login-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
